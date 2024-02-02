0934760e1218bcb2b86e5c35195a7b3aeefbd8c8
Kenshi2727 <123503901+Kenshi2727@users.noreply.github.com>
2024-02-02 22:56:07 +0530

    Update array.java
    hi hello good eveing

--- java/src/tutorial-codes/array.java
+++ java/src/tutorial-codes/array.java
@@ -1,9 +1,22 @@
-public class array 
+import java.util.Scanner;
+public class trian
 {
-    public static void main(String[] args) 
-    {
-      int nums[]={1,2,3,4,5};
-      for(int i=0;i<5;i++)
-      System.out.println(nums[i]);
-    }
+public static void main(String args[])
+{
+int i,j,n;    
+Scanner scan=new Scanner(System.in);    
+System.out.println("Enter the value of n:");
+n=scan.nextInt();
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+if(i+j>=(n-1)/2 && j-i<=(n-1)/2  && i<=(n-1)/2)
+System.out.print("* ");
+else
+System.out.print("  ");    
+}
+System.out.println();    
+}
+}    
 }
